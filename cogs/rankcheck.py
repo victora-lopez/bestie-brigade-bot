2050696abd4e40042218307077f720954cb5eb2d
Victor Lopez <victorandreslopez314@gmail.com>
2025-12-11 22:44:50 -0600

    Add match id fetch logic from trackergg api

--- cogs/rankcheck.py
+++ cogs/rankcheck.py
@@ -1,4 +1,6 @@
 from discord.ext import commands
+import cloudscraper
+
 
 class RivalsCog(commands.Cog):
     def __init__(self, bot):
@@ -9,12 +11,36 @@ class RivalsCog(commands.Cog):
             'gurt989': {'main': 'gurt gobain', 'smurf': 'yoboygurt'}
         }
         self.bot = bot
+        self.base_url = 'https://api.tracker.gg/api/v2/marvel-rivals/standard/'
 
-    @commands.command()
-    async def rankcheck(self, ctx):
-        user = ctx.author
+    @commands.command(name='rankcheck', aliases=['rc'])
+    async def rankcheck(self, ctx, account_type: str = None):
+        await ctx.send(f"Entered rankcheck function, {ctx.author} initiated")
+        user = ctx.author.name.lower()
         gamer_tag = self.gamer_tag_map.get(user)
+
         if gamer_tag is None:
-            await ctx.send(f'Couldn\'t find user\'s gamer tag')
+            await ctx.send(f'Couldn\'t find {user}\'s gamer tag')
         else:
-           pass
+            if account_type is None:
+                account_type = 'main'
+
+            gamer_tag = gamer_tag.get(account_type)
+
+            if gamer_tag is None:
+                await ctx.send(f'Invalid account type was entered please use one of the following: main, smurf, solo')
+            else:
+                url = f'{self.base_url}matches/ign/{gamer_tag}'
+                scaper = cloudscraper.create_scraper()
+                response = scaper.get(url, params={"season": "10"}, timeout=15)
+                data = response.json().get('data')
+                try:
+                    match_id = data.get('matches')[0].get('attributes').get('id')
+                except AttributeError:
+                    await ctx.send(f'Sorry there was an error fetching the match id due to a missing attribute. Tell victor to fix his bot.')
+                except Exception as e:
+                    await ctx.send(f'There was an error fetching match id: {e}')
+            a=5
+
+async def setup(bot):
+    await bot.add_cog(RivalsCog(bot))
